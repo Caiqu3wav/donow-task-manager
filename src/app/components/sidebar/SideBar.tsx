b1502eee7a27a26ee14d80acf1cde8bce54e7c80
Caique <asapcaique7@gmail.com>
2024-03-10 22:48:36 -0300

    init

---
+++ src/app/components/sidebar/SideBar.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from "react";
+import styled from "styled-components";
+export default function SideBar(){
+    return(
+        <div>
+            de
+        </div>
+    )
+}
+
+const SideBarStyled = styled.nav`
+
+`;
